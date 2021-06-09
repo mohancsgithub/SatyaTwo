becbf45842a60a40d46104e0610ba0b94306cd79
mohancsgithub <satyatohome@gmail.com>
2021-06-09 21:55:08 +0530

    added comment by mohan

--- MyFile.cs
+++ MyFile.cs
@@ -14,7 +14,7 @@ namespace WebApplication1.Models
         public string Location { get; set; }
         public string FatherName {get; set;}
         
-        //added mother name by user
+        //added mother name by user mohan
         public string MothernameInEng {get; set;}
     }
 }
